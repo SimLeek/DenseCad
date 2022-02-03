ab509adf69df4d42994c5c54c966d857e2f916fc
SimLeek <simulator.leek@gmail.com>
2022-02-03 15:55:12 -0700

    Added saving marching cubes code, text and zipped with generated model (ply).

--- marchingcad_gui.py
+++ marchingcad_gui.py
@@ -5,6 +5,8 @@ from pathlib import Path
 from tkinter import filedialog, messagebox
 from biocad.core import get_grid, transform, show_all
 import open3d as o3d
+import tempfile
+import shutil
 
 thresh = 1.0
 
@@ -85,7 +87,7 @@ def n_popup():
     toplevel.focus_force()
 
 
-def export_dialog():
+def export_callback():
     global cached_obs
     if cached_obs is None or len(cached_obs) == 0:
         try:
@@ -127,24 +129,82 @@ def export_dialog():
     messagebox.showinfo("Success", "Objects successfully exported!")
 
 
+def save_callback():
+    f = filedialog.asksaveasfile(
+        mode='w',
+        defaultextension='.zmarch',
+        filetypes=[
+            ('Marching Cubes Code', '*.march'),
+            ('Zipped Marching Cubes Code and PLY', '*.zmarch'),
+        ]
+    )
+
+    if f is None:  # dialog was closed with cancel
+        return
+
+    ext = pathlib.Path(f.name).suffix
+
+    text = t.get("1.0", 'end-1c')
+
+    if ext == '.march':
+        f.write(text)
+        f.close()
+    elif ext == '.zmarch':
+        fname = f.name
+        f.close()
+        stem = pathlib.Path(f.name).stem
+        march = stem + '.march'
+        stl = stem + '.ply'
+
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            try:
+                exec(text)
+                loc = list(locals().items())
+                cached_obs = [l[1] for l in loc if torch.is_tensor(l[1])]
+            except Exception as e:
+                messagebox.showerror("WARNING", "No objects are available to export. "
+                                                "generating current code gave error: " + str(e))
+            if len(cached_obs) == 0:
+                messagebox.showerror("WARNING", "No objects are available to export. "
+                                                "Code is error free, but generated no objects.")
+
+            full_stl = pathlib.Path(tmpdirname, stl)
+            mesh = core.get_showable(cached_obs, rgb)[0]
+            o3d.io.write_triangle_mesh(str(full_stl), mesh)
+
+            # todo: modify text to contain the warnings or checksum if generation succeeded
+            full_march = pathlib.Path(tmpdirname, march)
+            with open(full_march, mode='w') as f_march:
+                f_march.write(text)
+
+            shutil.make_archive(stem, 'zip', tmpdirname)
+            shutil.move(stem+'.zip', fname)
+    else:
+        messagebox.showerror("ERROR", f'"{ext}" is not a supported file type.')
+
+
 run_png = str(Path.joinpath(Path(__file__).parent, "gui_icons", "run.png"))
 clear_png = str(Path.joinpath(Path(__file__).parent, "gui_icons", "clear.png"))
 grid_png = str(Path.joinpath(Path(__file__).parent, "gui_icons", "grid.png"))
 export_png = str(Path.joinpath(Path(__file__).parent, "gui_icons", "export.png"))
+save_png = str(Path.joinpath(Path(__file__).parent, "gui_icons", "save.png"))
 
 run_photo = PhotoImage(file=run_png)
 clear_photo = PhotoImage(file=clear_png)
 grid_photo = PhotoImage(file=grid_png)
 export_photo = PhotoImage(file=export_png)
+save_photo = PhotoImage(file=save_png)
 
 Button(frm_top_buttons, text='Compile Object Code', image=run_photo, command=run_callback).pack(side=LEFT)
 Button(frm_top_buttons, text='Clear View', image=clear_photo).pack(side=LEFT)
 Button(frm_top_buttons, text='Setup Marching Cubes Grid', image=grid_photo, command=n_popup).pack(side=LEFT)
-Button(frm_top_buttons, text='Export Generated Object', image=export_photo, command=export_dialog).pack(side=LEFT)
+Button(frm_top_buttons, text='Export Generated Object', image=export_photo, command=export_callback).pack(side=LEFT)
+Button(frm_top_buttons, text='Save Object Code', image=save_photo, command=save_callback).pack(side=LEFT)
 
 root.bind('<Control-r>', run_callback)
 root.bind('<Control-n>', n_popup)
-root.bind('<Control-e>', run_callback)
+root.bind('<Control-e>', export_callback)
+root.bind('<Control-s>', save_callback)
 
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=1)
 # t.pack()
